cc0398474ea03f94346b31ce188cc65a3998f2ba
Rahul Sahani <devilken43@gmail.com>
2023-09-03 13:26:57 +0530

    Added more sounds

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import os
+import asyncio
 
 # Initialize pygame
 pygame.init()
@@ -35,6 +36,11 @@ background_music = pygame.mixer.Sound(os.path.join("sounds", "bloodpixelhero__re
 background_music.play(-1) 
 
 blaster_shoot_sound = pygame.mixer.Sound(os.path.join("sounds", "blaster-shot-single.wav"))
+
+ship_hit_sound = pygame.mixer.Sound(os.path.join("sounds", "thebuilder15__blast-wave.wav"))
+
+ship_blast_sound = pygame.mixer.Sound(os.path.join("sounds", "matrixxx__retro-explosion-07.wav"))
+
 
 # Player class
 class Player(pygame.sprite.Sprite):
@@ -121,44 +127,17 @@ def save_highscore(score):
     with open("highscore.txt", "w") as file:
         file.write(str(score))
 
-# ! Main game loop
-running = True
-clock = pygame.time.Clock()
+    # ! Main game loop
+async def main():
+    global lives, score
+    running = True
+    clock = pygame.time.Clock()
 
-game_over = False
+    game_over = False
 
-highscore = load_highscore()
+    highscore = load_highscore()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player.shoot()
-
-    if game_over:
-        screen.fill(BLACK)
-        game_over_text = font.render("Game Over", True, WHITE)
-        screen.blit(game_over_text, (SCREEN_WIDTH // 2 - 60, SCREEN_HEIGHT // 2 - 100))
-        score_text = font.render(f"Your Score: {score}", True, WHITE)
-        screen.blit(score_text, (SCREEN_WIDTH // 2 - 60, SCREEN_HEIGHT // 2 - 50))
-        highscore_text = font.render(f"Highscore: {highscore}", True, WHITE)
-        screen.blit(highscore_text, (SCREEN_WIDTH // 2 - 60, SCREEN_HEIGHT // 2 ))
-
-        exit_button = pygame.draw.rect(screen, WHITE, (SCREEN_WIDTH/2 - 50, 400, 100, 50))
-
-        exit_text = font.render("Exit", True, BLACK)
-        screen.blit(exit_text, (SCREEN_WIDTH/2 - 25, 415))
-
-        pygame.display.flip()
-
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if exit_button.collidepoint(mouse_pos):
-                    running = False
-    else:
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -166,62 +145,98 @@ while running:
                 if event.key == pygame.K_SPACE:
                     player.shoot()
 
-        # ! Update
-        all_sprites.update()
+        if game_over:
+            screen.fill(BLACK)
+            game_over_text = font.render("Game Over. Refresh the Page", True, WHITE)
+            screen.blit(game_over_text, (SCREEN_WIDTH // 2 - 60, SCREEN_HEIGHT // 2 - 100))
+            score_text = font.render(f"Your Score: {score}", True, WHITE)
+            screen.blit(score_text, (SCREEN_WIDTH // 2 - 60, SCREEN_HEIGHT // 2 - 50))
+            highscore_text = font.render(f"Highscore: {highscore}", True, WHITE)
+            screen.blit(highscore_text, (SCREEN_WIDTH // 2 - 60, SCREEN_HEIGHT // 2 ))
 
-        # Check for collisions between bullets and aliens
-        hits = pygame.sprite.groupcollide(bullets, aliens, True, True)
+            # exit_button = pygame.draw.rect(screen, WHITE, (SCREEN_WIDTH/2 - 50, 400, 100, 50))
 
-        # Check if a alien is destroyed and increase score
-        if len(hits) > 0:
-            score += 1
-    
+            # exit_text = font.render("Exit", True, BLACK)
+            # screen.blit(exit_text, (SCREEN_WIDTH/2 - 25, 415))
 
-        # Check if player gets an extra life from destroying aliens
-        if len(hits) > 0 and random.random() < 0.05:  # 5% chance
-            lives += 1
+            pygame.display.flip()
 
-        # Check if aliens reach the bottom
-        for alien in aliens:
-            if alien.rect.top > SCREEN_HEIGHT - 10:
+            # for event in pygame.event.get():
+            #     if event.type == pygame.MOUSEBUTTONDOWN:
+            #         mouse_pos = pygame.mouse.get_pos()
+            #         if exit_button.collidepoint(mouse_pos):
+            #             running = False
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        player.shoot()
+
+            # ! Update
+            all_sprites.update()
+
+            # Check for collisions between bullets and aliens
+            hits = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+            # Check if a alien is destroyed and increase score
+            if len(hits) > 0:
+                score += 1
+        
+
+            # Check if player gets an extra life from destroying aliens
+            if len(hits) > 0 and random.random() < 0.05:  # 5% chance
+                lives += 1
+
+            # Check if aliens reach the bottom
+            for alien in aliens:
+                if alien.rect.top > SCREEN_HEIGHT - 10:
+                    lives -= 1
+                    alien.kill()
+
+            # Check for collisions between player and aliens
+            hits_player = pygame.sprite.spritecollide(player, aliens, True)
+            if hits_player:
+                ship_hit_sound.play()
                 lives -= 1
-                alien.kill()
 
-        # Check for collisions between player and aliens
-        hits_player = pygame.sprite.spritecollide(player, aliens, True)
-        if hits_player:
-            lives -= 1
+            # Spawn new aliens
+            if len(aliens) < 5 and random.random() < 0.02:
+                new_alien = Alien()
+                all_sprites.add(new_alien)
+                aliens.add(new_alien)
 
-        # Spawn new aliens
-        if len(aliens) < 5 and random.random() < 0.02:
-            new_alien = Alien()
-            all_sprites.add(new_alien)
-            aliens.add(new_alien)
+            # Draw
+            screen.fill(BLACK)
+            all_sprites.draw(screen)
 
-        # Draw
-        screen.fill(BLACK)
-        all_sprites.draw(screen)
+            # Draw score and lives
+            score_text = font.render(f"HI-Score: {highscore}", True, WHITE)
+            screen.blit(score_text, (10, 10))
+            score_text = font.render(f"Score: {score}", True, WHITE)
+            screen.blit(score_text, (10, 50))
+            life_text = font.render(f"Lives: ", True, WHITE)
+            screen.blit(life_text, (10, 90))
 
-        # Draw score and lives
-        score_text = font.render(f"HI-Score: {highscore}", True, WHITE)
-        screen.blit(score_text, (10, 10))
-        score_text = font.render(f"Score: {score}", True, WHITE)
-        screen.blit(score_text, (10, 50))
-        life_text = font.render(f"Lives: ", True, WHITE)
-        screen.blit(life_text, (10, 90))
+            # Draw life icons
+            for i in range(lives):
+                screen.blit(life_image, (85 + i * 40, 85))
 
-        # Draw life icons
-        for i in range(lives):
-            screen.blit(life_image, (85 + i * 40, 85))
+            pygame.display.flip()
+            clock.tick(60)
 
-        pygame.display.flip()
-        clock.tick(60)
+            # Game over condition
+            if lives <= 0:
+                ship_blast_sound.play()
+                game_over = True
+                if score > highscore:
+                    highscore = score
+                    save_highscore(highscore)
+        await asyncio.sleep(0)
+    
+# Run the main function
+asyncio.run(main())
 
-        # Game over condition
-        if lives <= 0:
-            game_over = True
-            if score > highscore:
-                highscore = score
-                save_highscore(highscore)
-
+# Quit Pygame
 pygame.quit()
